ae125afb150bd4d00cf745f8c02042ea49f912a6
Anil Yadav <anilyadav@Anils-MacBook-Air.local>
2024-12-08 16:43:17 +0530

    product service

---
+++ src/main/java/com/example/productservice/controller/ProductController.java
@@ -0,0 +1,53 @@
+package com.example.productservice.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.productservice.entity.Product;
+import com.example.productservice.repository.ProductRepository;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+	@Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable Long id) {
+        return productRepository.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        if (productRepository.existsById(id)) {
+            product.setId(id);
+            return productRepository.save(product);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productRepository.deleteById(id);
+    }
+}
